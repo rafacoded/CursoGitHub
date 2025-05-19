30121d1dd756da115d269773ac6b8c9767b62ea1
rafacoded <rafalintirado50@gmail.com>
2025-05-19 09:39:45 +0200

    Se cambia el print en la función de division

--- src/main/java/com/example/unittest/Main.java
+++ src/main/java/com/example/unittest/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
     public static int divide(int a, int b) {
         if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
+            throw new IllegalArgumentException("No se puede dividir entre cero");
         }
 
         return a / b;
